327d987eb884dc771f0088329af7fe828e888075
Matheus G. Perez <sr.matheusgabriel.em@gmail.com>
2026-03-19 17:02:58 -0300

    Add files via upload

---
+++ conexao_wifi.ino
@@ -0,0 +1,52 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+
+const char* ssid = "Matheus Perez";
+const char* password = "123@abc.";
+
+const char* serverName = "https://jsonplaceholder.typicode.com/todos/1";
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando ao Wi-fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\nConectado com sucesso!");
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED){
+    WiFiClientSecure *cliente = new WiFiClientSecure;
+  }
+  if(cliente) {
+    cliente->setInsecure();
+
+    HTTPClient https;
+    Serial.print("[HTTPS] Iniciando requisição...\n");
+  }
+  if (https.begin(*cliente, serverName)) {
+    int httpCode = https.GET();
+
+    if(httpCode > 0){
+      Serial.printf("[HTTPS] Código de resposta: %d\n", httpCode);
+
+      if(httpCode == HTTP_CODE_OK){
+        String payload = https.getString();
+        Serial.println("Conteúdo recebido:");
+        Serial.println(payload);
+      }
+    } else {
+      Serial.printf("[HTTPS] Falha na requisição, erro: %s\n", https.errorToString(httpCode).c_str());
+    }
+  https.end();
+  } else {
+    Serial.printf("[HTTPS] Não foi possível conectar ao servidor\n");
+  }
+  delete cliente;
+  delay(10000);
+}
